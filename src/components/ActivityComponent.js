d4e09530dce7eebcadf180a11a1dee0acc36ef17
eduardisrael <israel_pasaca@icloud.com>
2020-12-20 13:50:18 -0500

    ClassComponent Activity and ProjectDetail to convert functional component js

--- src/components/ActivityComponent.js
+++ src/components/ActivityComponent.js
@@ -1,28 +1,40 @@
 import React from 'react';
 import { Card, CardImg, CardTitle } from 'reactstrap';
 
-class Activity extends React.Component {
   
-  render() {
-    const activity = this.props.projects.map((project) => {
-        return (
-          <div key={project.id} className="col-12 col-md-5 m-1">
-            <Card onClick={() => this.props.onClick(project.id)}>
-              <CardTitle className="text-justify">{project.name}</CardTitle>
-              <CardImg width="100%" object src={project.image} alt = {project.image} />
-            </Card>
-          </div>
-        );
-    });
+function RenderActivityItem ({project,onClick}){
+  return (
+    <Card onClick={() => onClick(project.id)}>
+      <CardTitle className="text-center">{project.name}</CardTitle>
+      <CardImg width="100%" object src={project.image} alt = {project.image} />
+    </Card>
+  );
+}
 
-    return (
-      <div className="container">
-        <div className="row">
-          {activity}
-        </div>
+
+//another way of implementing a functional component
+//this is the parameter that is going yo receive the props parameter
+//this function is going to return the menu and inside here
+//This is egere iterating over all the items
+//RenderActivityItem takes the parameters dish which is the dish that has received here, 2do onClick
+//the onClick which was sent in by the mainComp that parsing in to the render ActivityComponent as the function here
+const Activity = (props) => {
+  const activity = props.projects.map((project) => {
+    return(
+      <div key={project.id} className="col-12 col-md-5 m-1">
+        <RenderActivityItem project={project} onClick={props.onClick}/>
       </div>
     );
-  }
+  });
+
+  return (
+    <div className="container">
+      <div className="row">
+        {activity}
+      </div>
+    </div>
+  );
 }
+
   
   export default Activity;
--- src/components/ProjectDetailComponent.js
+++ src/components/ProjectDetailComponent.js
@@ -1,66 +1,62 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class ProjectDetail extends React.Component {
+//renderiza una nueva Card - function Component
+function RenderProject({project}) {
+  return(
+    <div className="col-12 col-md-5 m-1">
+      <Card>
+        <CardImg top src={project.image} alt={project.name} />
+        <CardBody>
+          <CardTitle className="text-center">{project.name}</CardTitle>
+          <CardText className="text-justify">{project.description}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
 
-  //renderiza una nueva Card
-  renderProject(project) {
-    return(
+//renderiza comentarios y fecha - function Component
+function RenderComments({comments}){
+  if (comments != null){
+    let  dateFormat = { year: "numeric", month:"short", day:"numeric"};
+    const commentsProjects = comments.map((comment) => {
+      return(
+        <li key = {comment.id}>
+          <p className="text-justify">{comment.comment}</p>
+          <p className="text-justify">-- {comment.author}, {new Date(comment.date).toLocaleDateString("en-US",dateFormat)}</p>
+        </li>
+      );
+    });
+
+    return (
       <div className="col-12 col-md-5 m-1">
-        <Card>
-          <CardImg top src={project.image} alt={project.name} />
-          <CardBody>
-            <CardTitle className="text-justify">{project.name}</CardTitle>
-            <CardText className="text-justify">{project.description}</CardText>
-          </CardBody>
-        </Card>
+        <h4 className="text-justify">Comments</h4>
+        <ul className="list-unstyled">
+          {commentsProjects}
+        </ul>
       </div>
     );
   }
-
-  //renderiza comentarios y fecha
-  renderComments(comments){
-    if (comments != null){
-      let  dateFormat = { year: "numeric", month:"short", day:"numeric"};
-      const commentsProjects = comments.map((comment) => {
-        return(
-          <li key = {comment.id}>
-            <p className="text-justify">{comment.comment}</p>
-            <p className="text-justify">-- {comment.author}, {new Date(comment.date).toLocaleDateString("en-US",dateFormat)}</p>
-          </li>
-        );
-      });
-
-      return (
-        <div className="col-12 col-md-5 m-1">
-          <h4 className="text-justify">Comments</h4>
-          <ul className="list-unstyled">
-            {commentsProjects}
-          </ul>
-        </div>
-      );
-    }
-    else {
-      return(<div></div>);
-    }
+  else {
+    return(<div></div>);
   }
+}
 
-  //Se renderiza la nueva vista
-  render(){
-    if ( this.props.project != null){
-      return(
-        <div class="container">
-            <div className="row">
-            {this.renderProject(this.props.project)}
-            {this.renderComments(this.props.project.comments)} 
-            </div>  
-        </div>   
-      );
-    }
-    else {
-      return(<div></div>);
-    }
-
+//Se renderiza la nueva vista
+const ProjectDetail = (props) => {
+  if (props.project != null){
+    return(
+      <div class="container">
+          <div className="row">
+          <RenderProject project = {props.project} />
+          <RenderComments comments = {props.project.comments}/>
+          </div>  
+      </div>   
+    );
+  }
+  else {
+    return(<div></div>);
   }
 }
 
